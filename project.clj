f68291e462623fa9962fac3586edfca16ac77139
Aria Haghighi <me@aria42.com>
2013-01-13 00:31:06 -0800

    prismatic organization

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dommy "0.0.1"
+(defproject prismatic/dommy "0.0.1"
   :description "No nonsense Clojurescript dom templating and (soon) manipulation"
   :plugins [[lein-cljsbuild "0.2.10"]]
   :dependencies [[crate "0.2.3" :scope "dev"]] ;; for perf test
