e2bce9a0e84bfd267c7c8ae6dd83b6adf439f1f0
Conner Petzold <cpetzold@gmail.com>
2013-01-16 15:20:51 -0800

    add oss info to project

--- project.clj
+++ project.clj
@@ -1,5 +1,14 @@
 (defproject prismatic/dommy "0.0.1"
   :description "No nonsense Clojurescript dom templating and (soon) manipulation"
+  :url "https://github.com/prismatic/dommy"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:prismatic/dommy.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Prismatic"]
+                              [:url "http://prismatic.com"]
+                              [:email "admin+oss@prismatic.com"]
+                              [:timezone "-8"]]]
   :plugins [[lein-cljsbuild "0.2.10"]]
   :dependencies [[crate "0.2.3" :scope "dev"]] ;; for perf test
   :hooks [leiningen.cljsbuild]
