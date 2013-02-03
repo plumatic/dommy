43372f2190548b83b3203097f799671f37bcf747
Aria Haghighi <me@aria42.com>
2013-02-03 11:47:28 -0800

    declare clojurescript in project.clj

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
                               [:email "admin+oss@getprismatic.com"]
                               [:timezone "-8"]]]
   :plugins [[lein-cljsbuild "0.2.10"]]
+  :clojurescript? true
   :dependencies [[crate "0.2.3" :scope "dev"]] ;; for perf test
   :hooks [leiningen.cljsbuild]
   :cljsbuild 
