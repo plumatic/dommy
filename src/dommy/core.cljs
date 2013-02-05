a91b2a587dc43530deba3c42c0888e827c3eea2b
Conner Petzold <cpetzold@gmail.com>
2013-02-04 16:13:58 -0800

    use classList instead of className

--- src/dommy/core.cljs
+++ src/dommy/core.cljs
@@ -28,77 +28,35 @@
   (.replaceChild (.-parentNode node) new node)
   new)
 
-(defn- selector [data]
-  (cond
-   (coll? data) (clojure.string/join " " (map selector data))
-   (or (string? data) (keyword? data)) (name data)))
-
-(defn- class-match?
-  "does class-name string have class starting at index idx"
-  [class-name class idx]
-  (and
-   ;; start
-   (or (zero? idx) (identical? \space (.charAt class-name (dec idx))))
-   ;; stop
-   (let [total-len (.-length class-name)
-         stop (+ idx (.-length class))]
-     (when (<= stop total-len)
-       (or (identical? stop total-len)
-           (identical? \space (.charAt class-name stop)))))))
-
-(defn- class-index
-  "Finds the index of class in a space-delimited class-name"
-  [class-name class]
-  (loop [start-from 0]
-    (let [i (.indexOf class-name class start-from)]
-      (when (>= i 0)
-        (if (class-match? class-name class i)
-          i
-          (recur (+ i (.-length class))))))))
-
 (defn has-class?
   "does DOM node have class"
   [node class]
-  (when-let [i (class-index (.-className node) class)]
-    (>= i 0)))
+  (.contains (.-classList node) class))
 
 (defn add-class!
-  "add class to node c"
-  [node c]
-  (let [class-name (.-className node)]
-    (when-not (class-index class-name c)
-      (set! (.-className node)
-            (if (identical? class-name "")
-              c
-              (str class-name " " c))))))
-
-(defn- remove-class-str [init-class-name class]
-  (loop [class-name init-class-name]
-    (let [class-len (.-length class-name)]
-      (if-let [i (class-index class-name class)]
-        (recur (let [end (+ i (.-length class))]
-                 (str (if (< end class-len)                        
-                        (str (.substring class-name 0 i) (.substr class-name (inc end)))
-                        (.substring class-name 0 (dec i))))))
-        class-name))))
+  "add class to node"
+  [node class]
+  (.add (.-classList node) class))
 
 (defn remove-class!
   "remove class from node"
   [node class]
-  (let [class-name (.-className node)
-        new-class-name (remove-class-str class-name (name class))]
-    (when-not (identical? class-name new-class-name)
-      (set! (.-className node) new-class-name))))
+  (.remove (.-classList node) class))
 
 (defn toggle-class!
   "(toggle-class! node class) will add-class! if node does not have class and remove-class! otherwise.
    (toggle-class! node class add?) will add-class! if add? is truthy, otherwise it will remove-class!"
   ([node class]
-     (toggle-class! node class (not (has-class? node class))))
+     (.toggle (.-classList node) class))
   ([node class add?]
      (if add?
        (add-class! node class)
        (remove-class! node class))))
+
+(defn- selector [data]
+  (cond
+   (coll? data) (clojure.string/join " " (map selector data))
+   (or (string? data) (keyword? data)) (name data)))
 
 (defn live-listener
   "fires f if event.target is found within the specified selector"
@@ -106,6 +64,12 @@
   (fn [event]
     (when (-> (sel node selector) (#(apply array %)) (.indexOf (.-target event)) (>= 0))
       (f event))))
+
+(defn listen!
+  ([node event-type live-selector f]
+     (listen! node event-type (live-listener node live-selector f)))
+  ([node event-type f]
+     (.addEventListener node (name event-type) f)))
 
 ;; (defprotocol PEventListener
 ;;   (listener-id [this] "id for the listener"))
@@ -117,8 +81,4 @@
 ;;     IFn
 ;;     (invoke [& args] (apply f args))))
 
-(defn listen!
-  ([node event-type live-selector f]
-     (listen! node event-type (live-listener node live-selector f)))
-  ([node event-type f]
-     (.addEventListener node (name event-type) f)))
+
