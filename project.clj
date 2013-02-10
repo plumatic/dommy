a9bb4c17488fc2c8077ad0556412d714fb9c0880
Aria Haghighi <me@aria42.com>
2013-02-09 18:46:42 -0800

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/dommy "0.0.2"
+(defproject prismatic/dommy "0.0.3-SNAPSHOT"
   :description "No nonsense Clojurescript dom templating and (soon) manipulation"
   :url "https://github.com/prismatic/dommy"
   :license {:name "Eclipse Public License"
