38271c825c050c6888b657eb89113fbd472cc779
Han <laughinghan@gmail.com>
2013-03-01 11:04:13 -0800

    add optional update-event! arg to fire!

--- test/dommy/core_test.cljs
+++ test/dommy/core_test.cljs
@@ -113,14 +113,18 @@
       (is (not (dommy/descendant? child sibling))))))
 
 (defn fire!
-  "Only works when `node` is in the DOM"
-  [node event-type]
-  (if (.-createEvent js/document)
-    (let [event (.createEvent js/document "Event")]
-      (.initEvent event (name event-type) true true)
-      (.dispatchEvent node event))
-    (.fireEvent node (str "on" (name event-type))
-                (.createEventObject js/document))))
+  "Creates an event of type `event-type`, optionally having
+   `update-event!` mutate and return an updated event object,
+   and fires it on `node`.
+   Only works when `node` is in the DOM"
+  [node event-type & [update-event!]]
+  (let [update-event! (or update-event! identity)]
+    (if (.-createEvent js/document)
+      (let [event (.createEvent js/document "Event")]
+        (.initEvent event (name event-type) true true)
+        (.dispatchEvent node (update-event! event)))
+      (.fireEvent node (str "on" (name event-type))
+                  (update-event! (.createEventObject js/document))))))
 
 (deftest live-listener
   (let [el-nested (node [:ul])
