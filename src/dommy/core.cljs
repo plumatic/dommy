f39cbe72dabb419d2f43d155b3a051ae95db5c8f
Benjamin Jenks <jenksb@gmail.com>
2013-05-05 21:00:48 +0100

    Removed duplicated style and set-style! defs

--- src/dommy/core.cljs
+++ src/dommy/core.cljs
@@ -20,8 +20,6 @@
 (def add-class! attrs/add-class!)
 (def remove-class! attrs/remove-class!)
 (def toggle-class! attrs/toggle-class!)
-(def set-style! attrs/set-style!)
-(def style attrs/style)
 (def set-attr! attrs/set-attr!)
 (def set-style! attrs/set-style!)
 (def set-px! attrs/set-px!)
