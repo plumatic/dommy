0da724e15c6df7a91175f734c09bc3c51c1ae6e5
Conner Petzold <cpetzold@gmail.com>
2014-01-16 16:29:59 -0800

    switch to clojurescript.test and fix tests

--- project.clj
+++ project.clj
@@ -10,19 +10,23 @@
                               [:url "http://getprismatic.com"]
                               [:email "admin+oss@getprismatic.com"]
                               [:timezone "-8"]]]
-  :plugins [[lein-cljsbuild "0.3.2"]]
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [com.cemerick/clojurescript.test "0.2.1"]
+            [com.cemerick/austin "0.1.3"]]
   :hooks [leiningen.cljsbuild]
   :dependencies [[crate "0.2.3" :scope "dev"] ;; for perf test
-                 [prismatic/cljs-test "0.0.6"]]
+                 [com.cemerick/clojurescript.test "0.2.1"]]
   :cljsbuild
-    {:builds
-     {:dev  {:source-paths ["src"]
-             :compiler {:output-to "target/main.js"
-                        :optimizations :whitespace
-                        :pretty-print true}}
-      :test {:source-paths ["src" "test"]
-             :incremental? true
-             :compiler {:output-to "target/unit-test.js"
-                        :optimizations :whitespace
-                        :pretty-print true}}}
-     :test-commands {"unit" ["phantomjs" "target/unit-test.js" "resources/test.html"]}})
+  {:builds
+   {:dev  {:source-paths ["src"]
+           :compiler {:output-to "target/main.js"
+                      :optimizations :whitespace
+                      :pretty-print true}}
+    :test {:source-paths ["src" "test"]
+           :incremental? true
+           :compiler {:output-to "target/unit-test.js"
+                      :optimizations :whitespace
+                      :pretty-print true}}}
+   :test-commands {"unit" ["phantomjs" :runner
+                           "window.literal_js_was_evaluated=true"
+                           "target/unit-test.js"]}})
